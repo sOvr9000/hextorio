bcf8904e94a09b07b15d22ad1e024bc1c996c4d8
sOvr9000 <jordanthegeek@gmail.com>
2025-05-15 16:37:56 -0700

    Custom inputs

---
+++ prototypes/custom_inputs.lua
@@ -0,0 +1,13 @@
+
+data:extend({
+    {
+        type = "custom-input",
+        name = "teleport-to-hex-core",
+        key_sequence = "T",
+    },
+    {
+        type = "custom-input",
+        name = "claim-hex-core",
+        key_sequence = "Y",
+    },
+})
