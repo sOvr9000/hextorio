23fd5a4c159304c94b742f9a04178744fd6ce82e
sOvr9000 <jordanthegeek@gmail.com>
2025-05-25 03:27:47 -0700

    Buff destroyers

---
+++ buff_destroyer.lua
@@ -0,0 +1,31 @@
+
+-- Buff destroyer bots
+local destroyer = data.raw["combat-robot"]["destroyer"]
+destroyer.attack_parameters = table.deepcopy(data.raw["electric-turret"]["tesla-turret"].attack_parameters)
+destroyer.attack_parameters.damage_modifier = 0.25
+destroyer.attack_parameters.range = 27
+destroyer.time_to_live = 14400 -- 2 minutes, double vanilla flavor
+destroyer.localised_name = {"item-name.tesla-destroyer"}
+destroyer.speed = 0.05
+destroyer.friction = 0.05
+
+-- Adjust recipe
+local recipe = data.raw["recipe"]["destroyer-capsule"]
+recipe.category = "electromagnetics"
+table.insert(recipe.ingredients, {type = "item", name = "tesla-ammo", amount = 4})
+
+-- Rebalance tech
+local tech = data.raw["technology"]["destroyer"]
+local tesla_tech = data.raw["technology"]["tesla-weapons"]
+tech.unit = {
+    count = 2000,
+    ingredients = tesla_tech.unit.ingredients,
+    time = tesla_tech.unit.time,
+}
+tech.prerequisites = {"tesla-weapons"}
+tech.localised_name = {"item-name.tesla-destroyer"}
+tech.localised_description = {"item-description.tesla-destroyer"}
+
+-- Adjust other localization
+local capsule = data.raw["capsule"]["destroyer-capsule"]
+capsule.localised_name = {"item-name.tesla-destroyer-capsule"}
--- data.lua
+++ data.lua
@@ -1,6 +1,7 @@
 
 require "prototypes.init"
 require "buff_discharge"
+require "buff_destroyer"
 
 -- Override title screen music.
 -- data.raw["ambient-sound"]["main-menu"].sound = "__hextorio__/sound/somethingwhatever.ogg"
