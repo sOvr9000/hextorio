b23f41cbc7058e1a85426ff29c2813f0872a2702
sOvr9000 <jordanthegeek@gmail.com>
2025-06-06 04:09:16 -0700

    Make dungeon turrets heal

--- prototypes/dungeon.lua
+++ prototypes/dungeon.lua
@@ -74,6 +74,7 @@ local railgun_damage_boost = 0.25 -- It one-shots you if not nerfed.
 local dungeon_laser_turret = table.deepcopy(data.raw["electric-turret"]["laser-turret"])
 dungeon_laser_turret.name = "dungeon-laser-turret"
 dungeon_laser_turret.energy_source = void_source
+dungeon_laser_turret.healing_per_tick = 0.25
 boost_max_health(dungeon_laser_turret, max_health_boost)
 boost_resistances(dungeon_laser_turret, {"electric"}, resistance_percentage_boost)
 boost_resistances(dungeon_laser_turret, {"fire"}, fire_resistance_percentage_boost)
@@ -84,6 +85,7 @@ boost_damage(dungeon_laser_turret, damage_boost)
 local dungeon_tesla_turret = table.deepcopy(data.raw["electric-turret"]["tesla-turret"])
 dungeon_tesla_turret.name = "dungeon-tesla-turret"
 dungeon_tesla_turret.energy_source = void_source
+dungeon_tesla_turret.healing_per_tick = 0.5
 boost_max_health(dungeon_tesla_turret, max_health_boost)
 boost_resistances(dungeon_tesla_turret, {"physical", "electric"}, resistance_percentage_boost)
 boost_resistances(dungeon_tesla_turret, {"fire"}, fire_resistance_percentage_boost)
@@ -95,6 +97,7 @@ local dungeon_railgun_turret = table.deepcopy(data.raw["ammo-turret"]["railgun-t
 dungeon_railgun_turret.name = "dungeon-railgun-turret"
 dungeon_railgun_turret.energy_source = nil
 dungeon_railgun_turret.energy_per_shot = nil
+dungeon_railgun_turret.healing_per_tick = 2.5
 boost_max_health(dungeon_railgun_turret, max_health_boost)
 boost_resistances(dungeon_railgun_turret, {"physical", "electric"}, resistance_percentage_boost)
 boost_resistances(dungeon_railgun_turret, {"fire"}, fire_resistance_percentage_boost)
@@ -105,6 +108,7 @@ dungeon_railgun_turret.heating_energy = "0J"
 
 local dungeon_gun_turret = table.deepcopy(data.raw["ammo-turret"]["gun-turret"])
 dungeon_gun_turret.name = "dungeon-gun-turret"
+dungeon_gun_turret.healing_per_tick = 0.2
 boost_max_health(dungeon_gun_turret, max_health_boost)
 boost_resistances(dungeon_gun_turret, {"physical"}, resistance_percentage_boost)
 boost_resistances(dungeon_gun_turret, {"fire"}, fire_resistance_percentage_boost)
@@ -115,6 +119,7 @@ dungeon_gun_turret.heating_energy = "0J"
 
 local dungeon_flamethrower_turret = table.deepcopy(data.raw["fluid-turret"]["flamethrower-turret"])
 dungeon_flamethrower_turret.name = "dungeon-flamethrower-turret"
+dungeon_flamethrower_turret.healing_per_tick = 0.4
 boost_max_health(dungeon_flamethrower_turret, max_health_boost)
 boost_resistances(dungeon_flamethrower_turret, {"physical"}, resistance_percentage_boost)
 boost_resistances(dungeon_flamethrower_turret, {"fire"}, fire_resistance_percentage_boost)
@@ -125,6 +130,7 @@ dungeon_flamethrower_turret.heating_energy = "0J"
 
 local dungeon_rocket_turret = table.deepcopy(data.raw["ammo-turret"]["rocket-turret"])
 dungeon_rocket_turret.name = "dungeon-rocket-turret"
+dungeon_rocket_turret.healing_per_tick = 1
 boost_max_health(dungeon_rocket_turret, max_health_boost)
 boost_resistances(dungeon_rocket_turret, {"physical", "electric"}, resistance_percentage_boost)
 boost_resistances(dungeon_rocket_turret, {"fire"}, fire_resistance_percentage_boost)
@@ -136,6 +142,7 @@ dungeon_rocket_turret.heating_energy = "0J"
 local dungeon_artillery_turret = table.deepcopy(data.raw["artillery-turret"]["artillery-turret"])
 dungeon_artillery_turret.name = "dungeon-artillery-turret"
 dungeon_artillery_turret.turret_rotation_speed = dungeon_artillery_turret.turret_rotation_speed * 4
+dungeon_artillery_turret.healing_per_tick = 2
 boost_max_health(dungeon_artillery_turret, max_health_boost)
 boost_resistances(dungeon_artillery_turret, {"physical", "electric"}, resistance_percentage_boost)
 boost_resistances(dungeon_artillery_turret, {"fire"}, fire_resistance_percentage_boost)
@@ -144,6 +151,7 @@ dungeon_artillery_turret.heating_energy = "0J"
 
 local dungeon_wall = table.deepcopy(data.raw["wall"]["stone-wall"])
 dungeon_wall.name = "dungeon-wall"
+dungeon_wall.healing_per_tick = 1
 boost_max_health(dungeon_wall, wall_max_health_boost)
 boost_resistances(dungeon_wall, {"physical"}, wall_physical_resistance_percentage_boost)
 boost_resistances(dungeon_wall, {"electric"}, 1)
