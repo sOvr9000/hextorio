70a862a089500eee08bff36922e777bd11ce4723
sOvr9000 <jordanthegeek@gmail.com>
2025-11-17 19:39:01 -0800

    Add buffs

--- data/item_buffs.lua
+++ data/item_buffs.lua
@@ -1,13 +1,6 @@
 
 return {
     item_buffs = {
-        ["copper-cable"] = {
-            {
-                type = "crafting-speed",
-                value = 0.05,
-                level_scaling = 1.1,
-            },
-        },
         ["iron-ore"] = {
             {
                 type = "mining-speed",
@@ -50,11 +43,32 @@ return {
                 level_scaling = 1.1,
             },
         },
+        ["tungsten-plate"] = {
+            {
+                type = "recipe-productivity",
+                values = {"tungsten-plate", 0.05},
+                level_scaling = 1.05,
+            },
+        },
+        ["scrap"] = {
+            {
+                type = "mining-productivity",
+                value = 0.05,
+                level_scaling = 1.05,
+            },
+        },
         ["holmium-ore"] = {
             {
                 type = "recipe-productivity",
                 values = {"scrap-recycling", 0.10},
-                level_scaling = 1.1,
+                level_scaling = 1.13,
+            },
+        },
+        ["holmium-plate"] = {
+            {
+                type = "recipe-productivity",
+                values = {"holmium-plate", 0.05},
+                level_scaling = 1.05,
             },
         },
         ["iron-plate"] = {
@@ -75,13 +89,6 @@ return {
             {
                 type = "inventory-size",
                 value = 1,
-                level_scaling = 1.4,
-            },
-        },
-        ["tungsten-plate"] = {
-            {
-                type = "inventory-size",
-                value = 2,
                 level_scaling = 1.4,
             },
         },
@@ -242,15 +249,225 @@ return {
         ["iron-gear-wheel"] = {
             {
                 type = "crafting-speed",
-                value = 0.05,
+                value = 0.10,
                 level_scaling = 1.1,
             },
         },
         ["iron-stick"] = {
             {
                 type = "crafting-speed",
+                value = 0.10,
+                level_scaling = 1.1,
+            },
+        },
+        ["copper-cable"] = {
+            {
+                type = "crafting-speed",
                 value = 0.05,
                 level_scaling = 1.1,
+            },
+        },
+        ["firearm-magazine"] = {
+            {
+                type = "physical-damage",
+                value = 0.04,
+                level_scaling = 1.1,
+            },
+        },
+        ["piercing-rounds-magazine"] = {
+            {
+                type = "physical-damage",
+                value = 0.04,
+                level_scaling = 1.1,
+            },
+        },
+        ["uranium-rounds-magazine"] = {
+            {
+                type = "physical-damage",
+                value = 0.04,
+                level_scaling = 1.1,
+            },
+        },
+        ["magmatic-rounds-magazine"] = {
+            {
+                type = "physical-damage",
+                value = 0.08,
+                level_scaling = 1.1,
+            },
+            {
+                type = "fire-damage",
+                value = 0.08,
+                level_scaling = 1.1,
+            },
+        },
+        ["gun-turret"] = {
+            {
+                type = "recipe-productivity",
+                values = {"firearm-magazine", 0.05},
+                level_scaling = 1.05,
+            },
+            {
+                type = "recipe-productivity",
+                values = {"piercing-rounds-magazine", 0.05},
+                level_scaling = 1.05,
+            },
+            {
+                type = "recipe-productivity",
+                values = {"uranium-rounds-magazine", 0.05},
+                level_scaling = 1.05,
+            },
+        },
+        ["rocket-turret"] = {
+            {
+                type = "recipe-productivity",
+                values = {"rocket", 0.05},
+                level_scaling = 1.05,
+            },
+        },
+        ["laser-turret"] = {
+            {
+                type = "laser-damage",
+                value = 0.04,
+                level_scaling = 1.1,
+            },
+        },
+        ["flamethrower-turret"] = {
+            {
+                type = "recipe-productivity",
+                values = {"heavy-oil-cracking-to-light-oil", 0.05},
+                level_scaling = 1.05,
+            },
+        },
+        ["tesla-turret"] = {
+            {
+                type = "electric-damage",
+                value = 0.04,
+                level_scaling = 1.1,
+            },
+        },
+        ["teslagun"] = {
+            {
+                type = "electric-shooting-speed",
+                value = 0.04,
+                level_scaling = 1.1,
+            },
+        },
+        ["submachine-gun"] = {
+            {
+                type = "bullet-shooting-speed",
+                value = 0.04,
+                level_scaling = 1.1,
+            },
+        },
+        ["electronic-circuit"] = {
+            {
+                type = "recipe-productivity",
+                values = {"electronic-circuit", 0.08},
+                level_scaling = 1.07,
+            },
+        },
+        ["productivity-module"] = {
+            {
+                type = "trade-productivity",
+                value = 0.01,
+                level_scaling = 1.07,
+            },
+        },
+        ["productivity-module-2"] = {
+            {
+                type = "trade-productivity",
+                value = 0.01,
+                level_scaling = 1.07,
+            },
+        },
+        ["productivity-module-3"] = {
+            {
+                type = "trade-productivity",
+                value = 0.02,
+                level_scaling = 1.07,
+            },
+        },
+        ["hexa-productivity-module"] = {
+            {
+                type = "trade-productivity",
+                value = 0.04,
+                level_scaling = 1.10,
+            },
+        },
+        ["speed-module"] = {
+            {
+                type = "crafting-speed",
+                value = 0.20,
+                level_scaling = 1.10,
+            },
+        },
+        ["speed-module-2"] = {
+            {
+                type = "crafting-speed",
+                value = 0.20,
+                level_scaling = 1.10,
+            },
+        },
+        ["speed-module-3"] = {
+            {
+                type = "crafting-speed",
+                value = 0.30,
+                level_scaling = 1.10,
+            },
+        },
+        ["hexa-speed-module"] = {
+            {
+                type = "crafting-speed",
+                value = 0.50,
+                level_scaling = 1.10,
+            },
+        },
+        ["efficiency-module"] = {
+            {
+                type = "recipe-productivity",
+                values = {"electronic-circuit", 0.08},
+                level_scaling = 1.07,
+            },
+            {
+                type = "all-buffs-cost-reduced",
+                value = 0.02,
+                level_scaling = 1.05,
+            },
+        },
+        ["efficiency-module-2"] = {
+            {
+                type = "recipe-productivity",
+                values = {"advanced-circuit", 0.08},
+                level_scaling = 1.07,
+            },
+            {
+                type = "all-buffs-cost-reduced",
+                value = 0.02,
+                level_scaling = 1.05,
+            },
+        },
+        ["efficiency-module-3"] = {
+            {
+                type = "recipe-productivity",
+                values = {"processing-unit", 0.08},
+                level_scaling = 1.07,
+            },
+            {
+                type = "all-buffs-cost-reduced",
+                value = 0.03,
+                level_scaling = 1.05,
+            },
+        },
+        ["hexa-efficiency-module"] = {
+            {
+                type = "recipe-productivity",
+                values = {"quantum-processor", 0.08},
+                level_scaling = 1.07,
+            },
+            {
+                type = "all-buffs-cost-reduced",
+                value = 0.05,
+                level_scaling = 1.05,
             },
         },
         ["logistic-robot"] = {
@@ -293,11 +510,33 @@ return {
                 level_scaling = 1.1,
             },
         },
+        ["lithium-plate"] = {
+            {
+                type = "inventory-size",
+                value = 5,
+                level_scaling = 1.3,
+            },
+        },
+        ["hexaprism"] = {
+            {
+                type = "all-buffs-level",
+                value = 1,
+                level_scaling = 1.12,
+            },
+        },
+        ["sentient-spider"] = {
+            {
+                type = "all-buffs-level",
+                value = 1,
+                level_scaling = 1.12,
+            },
+        },
     },
 
     show_as_linear = { -- Numbers aren't percentages for these buff types
         ["reach-distance"] = true,
         ["inventory-size"] = true,
+        ["all-buffs-level"] = true,
     },
 
     unlocked = {},
