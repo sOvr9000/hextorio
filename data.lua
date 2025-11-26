45311a408795ad68abf104d26375edd836ee7894
sOvr9000 <jordanthegeek@gmail.com>
2025-11-26 15:16:52 -0800

    Fix title screen music crash

--- data.lua
+++ data.lua
@@ -6,8 +6,14 @@ require "buff_defender"
 require "buff_distractor"
 require "prototypes.init"
 
+local lib = require "api.lib"
+
 -- Override title screen music.
-local title_screen_music = require("api.lib").startup_setting_value "title-screen-music"
-if title_screen_music ~= "vanilla" then
-    data.raw["ambient-sound"]["main-menu"].sound = "__hextorio__/sound/" .. title_screen_music .. ".ogg"
+if data.raw["ambient-sound"]["main-menu"] == nil then
+    lib.log_error("data.lua: Could not find main menu ambient sound data. Another mod has removed it.")
+else
+    local title_screen_music = lib.startup_setting_value "title-screen-music"
+    if title_screen_music ~= "vanilla" then
+        data.raw["ambient-sound"]["main-menu"].sound = "__hextorio__/sound/" .. title_screen_music .. ".ogg"
+    end
 end
