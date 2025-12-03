16ef0290858d6feb2279852d3ec7d25a7e839553
sOvr9000 <jordanthegeek@gmail.com>
2025-12-03 12:20:47 -0800

    Give sentient spider phsyical resistance

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -412,6 +412,7 @@ sentient_spider.chain_shooting_cooldown_modifier = 3 / 20
 sentient_spider.chunk_exploration_radius = 4
 table.insert(sentient_spider.resistances, {type = "electric-hv", percent = 100})
 table.insert(sentient_spider.resistances, {type = "electric", percent = 100})
+table.insert(sentient_spider.resistances, {type = "physical", percent = 100})
 
 
 for _, surface_name in pairs {"nauvis", "vulcanus", "fulgora", "gleba", "aquilo"} do
