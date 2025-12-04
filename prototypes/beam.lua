08704ca35aa37e0cf46fe85142156d954e9eaea1
sOvr9000 <jordanthegeek@gmail.com>
2025-12-04 01:32:24 -0800

    Recolor electric arcs from EMP

--- prototypes/beam.lua
+++ prototypes/beam.lua
@@ -1,6 +1,9 @@
 
 local emp_beam_bounce = table.deepcopy(data.raw["beam"]["chain-tesla-turret-beam-bounce"])
 emp_beam_bounce.name = "chain-electromagnetic-penetrator-beam-bounce"
+emp_beam_bounce.graphics_set.ground.body.tint = {0.75, 0.05, 0.05}
+emp_beam_bounce.graphics_set.ground.head.tint = {0.75, 0.05, 0.05}
+emp_beam_bounce.graphics_set.ground.tail.tint = {0.75, 0.05, 0.05}
 emp_beam_bounce.action.action_delivery.target_effects = {
     type = "damage",
     damage = {
@@ -8,6 +11,17 @@ emp_beam_bounce.action.action_delivery.target_effects = {
         amount = 300,
     },
 }
+
+emp_beam_bounce.graphics_set.beam.start.filename = "__hextorio__/graphics/beam/chain-beam-START.png"
+emp_beam_bounce.graphics_set.beam.ending.filename = "__hextorio__/graphics/beam/chain-beam-END.png"
+emp_beam_bounce.graphics_set.beam.head.layers[1].filename = "__hextorio__/graphics/beam/chain-body-0.png"
+emp_beam_bounce.graphics_set.beam.head.layers[2].filename = "__hextorio__/graphics/beam/chain-body-1.png"
+emp_beam_bounce.graphics_set.beam.tail.layers[1].filename = "__hextorio__/graphics/beam/chain-body-0.png"
+emp_beam_bounce.graphics_set.beam.tail.layers[2].filename = "__hextorio__/graphics/beam/chain-body-6.png"
+for i = 1, 6 do
+    emp_beam_bounce.graphics_set.beam.body[i].layers[1].filename = "__hextorio__/graphics/beam/chain-body-0.png"
+    emp_beam_bounce.graphics_set.beam.body[i].layers[2].filename = "__hextorio__/graphics/beam/chain-body-" .. i .. ".png"
+end
 
 -- local emp_beam_start = table.deepcopy(data.raw["beam"]["chain-tesla-turret-beam-start"])
 -- emp_beam_start.name = "chain-electromagnetic-penetrator-beam-start"
