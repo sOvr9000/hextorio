69c2cfc5c667d0b55577be5c7cfdcc11244d6685
sOvr9000 <jordanthegeek@gmail.com>
2025-12-03 21:22:43 -0800

    Add iron ore item value

--- api/migrations.lua
+++ api/migrations.lua
@@ -552,6 +552,9 @@ local process_migration = {
         hex_grid.update_all_trades()
     end,
     ["1.2.1"] = function()
+        item_values.reset_storage()
+        storage.item_values.values = data_item_values.values
+
         storage.initialization = storage.events
         storage.events = nil
 
