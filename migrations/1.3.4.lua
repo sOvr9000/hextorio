9d01c5bdbf4310b8d6e63f78c6e1591481db469c
sOvr9000 <jordanthegeek@gmail.com>
2025-12-10 20:09:17 -0800

    migrations

--- migrations/1.3.4.lua
+++ migrations/1.3.4.lua
@@ -1,6 +1,14 @@
 
+local hex_grid = require "api.hex_grid"
 local train_trading = require "api.train_trading"
 
 return function()
     train_trading.init()
+
+    for surface_id, _ in pairs(storage.hex_grid.surface_hexes) do
+        for _, state in pairs(hex_grid.get_flattened_surface_hexes(surface_id)) do
+            -- Enable this by default.
+            state.send_outputs_to_cargo_wagons = true
+        end
+    end
 end
