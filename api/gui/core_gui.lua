2d1f4f12b0945c2dea00c73122625065cdd8aabb
sOvr9000 <jordanthegeek@gmail.com>
2026-01-17 01:57:34 -0800

    Improve description for all buffs cost reduced buff type

--- api/gui/core_gui.lua
+++ api/gui/core_gui.lua
@@ -466,6 +466,8 @@ function core_gui.get_buff_description_tooltip(buff_type)
         return {"item-buff-description." .. buff_type, breakdown_str, interval}
     elseif buff_type == "train-trading-capacity" then
         return {"item-buff-description." .. buff_type, "[font=heading-2][color=green]" .. storage.item_buffs.train_trading_capacity .. "[.color][.font]"}
+    elseif buff_type == "all-buffs-cost-reduced" then
+        return {"item-buff-description." .. buff_type, "[font=heading-2][color=green]" .. (math.floor(100 / storage.item_buffs.cost_multiplier) * 0.01) .. "[.color][.font]"}
     else
         return {"item-buff-description." .. buff_type}
     end
