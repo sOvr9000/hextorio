4e9ce0d7242e790560253f187560cde93ba70211
sOvr9000 <jordanthegeek@gmail.com>
2026-01-22 17:52:34 -0800

    Nerf magmatic flame lifetime

--- prototypes/fire.lua
+++ prototypes/fire.lua
@@ -2,6 +2,10 @@
 local magmatic_flame = table.deepcopy(data.raw["fire"]["fire-flame"])
 magmatic_flame.name = "magmatic-flame"
 magmatic_flame.damage_per_tick.amount = 12
+magmatic_flame.initial_lifetime = 120
+magmatic_flame.lifetime_increase_cooldown = 8
+magmatic_flame.lifetime_increase_by = 24
+magmatic_flame.maximum_lifetime = 240
 
 ---@diagnostic disable-next-line: assign-type-mismatch
 data:extend({magmatic_flame})
