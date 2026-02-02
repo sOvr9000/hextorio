a715db6644a7e4932671416143b78f73a33e8686
sOvr9000 <jordanthegeek@gmail.com>
2026-02-02 02:36:02 -0800

    Increase default Fulgora trades per hex

--- settings/trades.lua
+++ settings/trades.lua
@@ -66,7 +66,7 @@ data:extend({
         type = "int-setting",
         name = "hextorio-trades-per-hex-fulgora",
         setting_type = "runtime-global",
-        default_value = 3,
+        default_value = 4,
         minimum_value = 0,
         maximum_value = 10,
         order = "t[trades]-d[trades-per-hex]-q[fulgora]",
