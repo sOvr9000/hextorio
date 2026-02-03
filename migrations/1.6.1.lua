661937199c8c59a46c30af7cd22c21128315c21f
sOvr9000 <jordanthegeek@gmail.com>
2026-02-02 20:24:28 -0800

    migrations

--- migrations/1.6.1.lua
+++ migrations/1.6.1.lua
@@ -1,3 +1,15 @@
 
+local gameplay_statistics = require "api.gameplay_statistics"
+
 return function()
+    if not storage.trades.uniquely_traded_items then
+        storage.trades.uniquely_traded_items = {}
+    else
+        for item_name, _ in pairs(storage.trades.uniquely_traded_items) do
+            if type(item_name) == "string" then
+                storage.trades.uniquely_traded_items[item_name] = nil
+            end
+        end
+    end
+    gameplay_statistics.set("total-unique-items-traded", 0)
 end
