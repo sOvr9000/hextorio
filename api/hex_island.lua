9668373e7deb03fc0e40b8e3f73c9ad62c4e8106
sOvr9000 <jordanthegeek@gmail.com>
2026-02-03 12:35:51 -0800

    Make spiral world gen respect radius settings

--- api/hex_island.lua
+++ api/hex_island.lua
@@ -162,14 +162,21 @@ local island_generators = {
         hex_sets.add(island, pos)
 
         local dir = math.random(1, 6)
-        for i = 1, 10 do
+        local done = false
+        for i = 1, 999 do
+            if done then break end
             for j = 1, 3 do
                 dir = 1 + dir % 6
                 local offset = axial.get_adjacency_offset(dir)
                 for n = 1, i do
                     pos = axial.add(pos, offset)
+                    if axial.distance(pos, {q=0, r=0}) > radius then
+                        done = true
+                        break
+                    end
                     hex_sets.add(island, pos)
                 end
+                if done then break end
             end
         end
 
