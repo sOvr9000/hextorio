9cbee5f654ecf2bfbbd240bb8c6d7987f94c917d
sOvr9000 <jordanthegeek@gmail.com>
2026-02-03 11:06:22 -0800

    migrations

--- migrations/1.6.2.lua
+++ migrations/1.6.2.lua
@@ -1,2 +1,12 @@
 
-return function() end
+return function()
+    for _, surface in pairs(game.surfaces) do
+        local hex_core_loaders = surface.find_entities_filtered {
+            name = "hex-core-loader",
+        }
+
+        for _, loader in pairs(hex_core_loaders) do
+            loader.rotatable = true
+        end
+    end
+end
