b061cf01392698c40b6a2db73bd4f7ba96db9435
sOvr9000 <jordanthegeek@gmail.com>
2026-02-09 21:15:54 -0800

    Move "Too Many Belts" behind "Trades Galore"

--- data/quests.lua
+++ data/quests.lua
@@ -111,12 +111,6 @@ return {
             prerequisites = {"ground-zero"},
         },
         {
-            name = "too-many-belts",
-            conditions = {{type = "trades-found", progress_requirement = 2000}},
-            rewards = {{type = "unlock-feature", value = "locomotive-trading"}, {type = "claim-free-hexes", value = {"nauvis", 10}}},
-            prerequisites = {"find-some-trades"},
-        },
-        {
             name = "remind-me-later",
             conditions = {{type = "favorite-trade", progress_requirement = 1, show_progress_bar = true}},
             rewards = {
@@ -262,6 +256,12 @@ return {
             conditions = {{type = "trades-found", progress_requirement = 200}},
             rewards = {{type = "unlock-feature", value = "sink-mode", notes = {"irreversible-action", "low-efficiency"}}, {type = "claim-free-hexes", value = {"nauvis", 3}}},
             prerequisites = {"find-some-trades"},
+        },
+        {
+            name = "too-many-belts",
+            conditions = {{type = "trades-found", progress_requirement = 2000}},
+            rewards = {{type = "unlock-feature", value = "locomotive-trading"}, {type = "claim-free-hexes", value = {"nauvis", 10}}},
+            prerequisites = {"trades-galore"},
         },
         {
             name = "i-really-need-that",
