89fa9a8793f773e2357fe55fb8990abdba2babe1
sOvr9000 <jordanthegeek@gmail.com>
2026-02-16 21:26:22 -0800

    Fix migration

--- migrations/1.6.10.lua
+++ migrations/1.6.10.lua
@@ -1,22 +1,3 @@
 
-local item_value_solver = require "api.item_value_solver"
-local item_tradability_solver = require "api.item_tradability_solver"
-
 return function()
-    storage.SUPPORTED_PLANETS = {
-        nauvis = true,
-        vulcanus = true,
-        fulgora = true,
-        gleba = true,
-        aquilo = true,
-    }
-
-    storage.item_values = {
-        values = {nauvis = {}, vulcanus = {}, fulgora = {}, aquilo = {}},
-        awaiting_solver = true,
-        base_coin_value = 10,
-    }
-
-    item_tradability_solver.solve()
-    item_value_solver.run()
 end
