8bc514eb226c4797bfff33cbbe736bc37e18be62
sOvr9000 <jordanthegeek@gmail.com>
2026-02-21 11:21:56 -0800

    Suppress warning

--- migrations/0.1.4.lua
+++ migrations/0.1.4.lua
@@ -6,7 +6,7 @@ local hex_state_manager = require "api.hex_state_manager"
 
 return function()
     for item_name, _ in pairs(storage.item_values.values.nauvis) do
-        if lib.is_catalog_item(item_name) then
+        if lib.is_catalog_item("nauvis", item_name) then
             local rank = item_ranks.get_item_rank(item_name)
             if rank >= 2 then
                 hex_grid.apply_extra_trades_bonus_retro(item_name)
