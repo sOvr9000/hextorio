bdd2a95b40342bdc629b5fcdec98a536e7f465c9
sOvr9000 <jordanthegeek@gmail.com>
2026-04-24 02:04:48 -0700

    migrations

--- migrations/1.7.3.lua
+++ migrations/1.7.3.lua
@@ -1,6 +1,8 @@
 
 local item_tradability_solver = require "api.item_tradability_solver"
+local trades = require "api.trades"
 
 return function()
     item_tradability_solver.solve()
+    trades.queue_productivity_update_job()
 end
