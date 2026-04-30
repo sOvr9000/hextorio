6973365387879266addc7541b5a58feb97888333
sOvr9000 <jordanthegeek@gmail.com>
2026-04-30 16:29:29 -0700

    Remove logs

--- migrations/1.7.6.lua
+++ migrations/1.7.6.lua
@@ -5,21 +5,16 @@ local strongboxes = require "api.strongboxes"
 return function()
     local sb_entity_names = {}
     for _, prot in pairs(prototypes.entity) do
-        log(prot.name)
         if prot.name:sub(1, 15) == "strongbox-tier-" then
             sb_entity_names[#sb_entity_names+1] = prot.name
         end
     end
-
-    log(serpent.block(sb_entity_names))
 
     for _, surface in pairs(game.surfaces) do
         if lib.is_vanilla_planet_name(surface.name) then
             local sb_entities = surface.find_entities_filtered {
                 name = sb_entity_names,
             }
-            log(surface.name)
-            log(#sb_entities)
             for _, sb_entity in pairs(sb_entities) do
                 strongboxes.update_chart_tag(sb_entity)
             end
