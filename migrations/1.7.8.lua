686a0d6874997a73e3b07c5229958ab44a7cb80e
sOvr9000 <jordanthegeek@gmail.com>
2026-05-11 19:15:24 -0700

    Fix cross-planet teleportation warning about dungeon hexes

--- migrations/1.7.8.lua
+++ migrations/1.7.8.lua
@@ -1,13 +1,15 @@
 
 local hex_state_manager = require "api.hex_state_manager"
 local item_buffs = require "api.item_buffs"
+local dungeons = require "api.dungeons"
+local axial    = require "api.axial"
 
 local data_item_buffs = require "data.item_buffs"
 
 return function()
     -- Clean up any hex claim tool accidents.
-    for surface_name, _ in pairs(storage.hex_grid.surface_hexes) do
-        local surface_hexes = hex_state_manager.get_surface_hexes(surface_name)
+    for surface_id, _ in pairs(storage.hex_grid.surface_hexes) do
+        local surface_hexes = hex_state_manager.get_surface_hexes(surface_id)
         if surface_hexes then
             for _, Q in pairs(surface_hexes) do
                 for r, state in pairs(Q) do
