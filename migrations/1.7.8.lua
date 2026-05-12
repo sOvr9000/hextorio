8ae62ce5843ada9b5b74591d589817e90065318d
sOvr9000 <jordanthegeek@gmail.com>
2026-05-11 23:35:45 -0700

    Add version step 1.7.8 -> 1.7.9 -> 1.8.0

--- migrations/1.7.8.lua
+++ migrations/1.7.8.lua
@@ -1,18 +1,11 @@
 
 local hex_state_manager = require "api.hex_state_manager"
-local spider_network = require "api.spider_network"
 local item_buffs = require "api.item_buffs"
 local quests = require "api.quests"
 
 local data_item_buffs = require "data.item_buffs"
 
 return function()
-    for _, surface in pairs(game.surfaces) do
-        for _, e in pairs(surface.find_entities_filtered {name = "sentient-spider"}) do
-            spider_network.register_spider(e)
-        end
-    end
-
     -- Clean up any hex claim tool accidents.
     for surface_id, _ in pairs(storage.hex_grid.surface_hexes) do
         local surface_hexes = hex_state_manager.get_surface_hexes(surface_id)
