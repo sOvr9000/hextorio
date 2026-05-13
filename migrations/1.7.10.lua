0d4f96d22b1490cd1085e5436cdafae16280d662
sOvr9000 <jordanthegeek@gmail.com>
2026-05-12 19:28:48 -0700

    Move migration to 1.7.10

---
+++ migrations/1.7.10.lua
@@ -0,0 +1,10 @@
+
+local spider_network = require "api.spider_network"
+
+return function()
+    for _, surface in pairs(game.surfaces) do
+        for _, e in pairs(surface.find_entities_filtered {name = "sentient-spider"}) do
+            spider_network.register_spider(e)
+        end
+    end
+end
