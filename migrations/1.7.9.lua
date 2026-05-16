1c35b05517387d86e303da61cacb24b3bee24928
sOvr9000 <jordanthegeek@gmail.com>
2026-05-15 18:21:15 -0700

    Skip version 1.7.10

--- migrations/1.7.9.lua
+++ migrations/1.7.9.lua
@@ -1,3 +1,19 @@
 
+local spider_network = require "api.spider_network"
+local hex_state_manager = require "api.hex_state_manager"
+local hex_pathfinding   = require "api.hex_pathfinding"
+
 return function()
+    for _, surface in pairs(game.surfaces) do
+        local surface_index = surface.index
+
+        for _, e in pairs(surface.find_entities_filtered {name = "sentient-spider"}) do
+            spider_network.register_spider(e)
+        end
+
+        for _, state in pairs(hex_state_manager.get_flattened_surface_hexes(surface)) do
+            state.surface_index = surface_index
+            hex_pathfinding.recalculate_hex_traversability(state)
+        end
+    end
 end
